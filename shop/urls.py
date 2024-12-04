514177cc666d6ebd5f0fbdf377577ae3d2f332a5
Shaun <nhlamulosmashaba@gmail.com>
2024-12-04 09:44:26 +0200

    added view to serve images

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
 from django.urls import re_path
+from django.conf import settings 
+from django.views.static import serve
 
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
@@ -11,3 +13,11 @@ urlpatterns = [
     path('products/<int:pk>/update_stock/', views.update_stock, name='product-update-stock'),
     re_path(r'^media/(?P<path>.*)$', views.serve_media, name='serve_media')
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+
